b22b28a985d716ead6413b2f0afb43624034d531
Vedavyas5007 <157232858+Vedavyas5007@users.noreply.github.com>
2025-02-04 20:01:07 -0500

    final commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'todo';
+
+  task_text:string='';
+  tasks:string[]=[];
+  completed_tasks:string[]=[];
+
+  addText(event:any){
+    this.task_text=event.target.value;
+  //  this.tasks.push(event.target.value);
+  }
+  addTask(){
+
+    if(this.task_text==''){
+      alert("please enter a task");
+    }else{
+      this.tasks.push(this.task_text);
+      let v:any=document.getElementById('inp');
+      v.value='';
+      this.task_text='';
+    }
+    
+  }
+  deleteTask(index:any){
+    
+  }
+  moveTask(index:any){
+    this.completed_tasks.push(this.tasks[index]);
+    this.tasks.splice(index,1);
+  }
+}
